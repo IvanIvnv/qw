c75cff1bc0c78e998eb4709ee11f58feedd16df6
IvanIvnv <somali.land@yandex.ru>
2018-05-21 23:45:36 +0300

    1.1. Создание Hello World[#141]

---
+++ src/ru/qw/calculate/Clculaate.java
@@ -0,0 +1,18 @@
+package ru.qw.calculaate;
+
+/**
+ * Calculate.
+ *
+ * @author Ivan Makvic (somali.land@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
