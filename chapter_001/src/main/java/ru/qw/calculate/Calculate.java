221d953e1aaac8e0367ecfd6fd0e2dcb91b99c80
IvanIvnv <somali.land@yandex.ru>
2018-06-11 19:34:23 +0300

    1.2. Создание Hello World[#141]

--- chapter_001/src/main/java/ru/qw/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/qw/calculate/Calculate.java
@@ -1,18 +1,32 @@
+/**
+Calculator
+*/
+
 package ru.qw.calculate;
 
 /**
- * Calculate.
+ * класс Calculate решение задачи части 001 урок 1
  *
- * @author Ivan Makvic (somali.land@yandex.ru)
- * @version $Id$
- * @since 0.1
+ * @author makvic 
+ * @version 1
+ * @since 03.06.2018
  */
 public class Calculate {
     /**
-     * Main.
-     * @param args - args.
-     */
+	Main
+	@param args-args 
+	*/
+	
     public static void main(String[] args) {
         System.out.println("Hello World");
+    }
+	
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+	  public String echo(String name) {
+        return "Echo, echo, echo : " + name;
     }
 }
